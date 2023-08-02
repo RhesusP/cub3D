f0e5a28b063b0f99ebb78b7099f51364e36ea027
RhesusP <camil.bernot.cb@gmail.com>
2023-08-02 21:34:49 +0200

    handle wrong number of argument error

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,23 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:25:31 by cbernot           #+#    #+#             */
-/*   Updated: 2023/08/02 21:05:40 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/08/02 21:33:28 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/cub3d.h"
 
+int	print_error(char *str)
+{
+	ft_putstr_fd("\033[31mError\033[39m\n", 2);
+	ft_putstr_fd(str, 2);
+	return (EXIT_FAILURE);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	if (argc != 2)
+		return (print_error("Wrong number of arguments\n"));
 	(void)argv;
 	return (0);
 }
