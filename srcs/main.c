c45b9046ba32ce0b37e55acee0e96751142e7381
RhesusP <camil.bernot.cb@gmail.com>
2023-08-02 21:40:26 +0200

    handle wrong file extension error

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:25:31 by cbernot           #+#    #+#             */
-/*   Updated: 2023/08/02 21:33:28 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/08/02 21:39:15 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,27 @@ int	print_error(char *str)
 	return (EXIT_FAILURE);
 }
 
+int	is_file_extension_correct(char *filename)
+{
+	int	i;
+
+	i = 0;
+	while (filename[i])
+		i++;
+	if (i < 5)
+		return (0);
+	if (filename[i - 1] != 'b' || filename[i - 2] != 'u'
+		|| filename[i - 3] != 'c' || filename[i - 4] != '.')
+		return (0);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		return (print_error("Wrong number of arguments\n"));
+	if (!is_file_extension_correct(argv[1]))
+		return (print_error("Wrong file extension\n"));
 	(void)argv;
 	return (0);
 }
