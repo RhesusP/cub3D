9b3640e48b24f8701e42f016d5e4431e499de70e
RhesusP <camil.bernot.cb@gmail.com>
2023-08-17 17:31:53 +0200

    delete debug functions

--- srcs/map_utils.c
+++ srcs/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: cbernot <cbernot@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 10:12:56 by cbernot           #+#    #+#             */
-/*   Updated: 2023/08/16 12:28:53 by cbernot          ###   ########.fr       */
+/*   Updated: 2023/08/17 17:31:32 by cbernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,35 +69,4 @@ int	is_map_desc(char *line)
 		return (0);
 	else
 		return (1);
-}
-
-/**
- * @brief [DEBUG FUNCTION] Print the map info structure
- * 
- * @param map 
- */
-void	debug_print_map(t_map_info *map)
-{
-	int i;
-	
-	if (map->floor_color == -1)
-		printf("Floor color: UNDEFINED\n");
-	else
-		printf("Floor color: %d\n", map->floor_color);
-	if (map->ceiling_color == -1)
-		printf("Ceiling color: UNDEFINED\n");
-	else
-		printf("Ceiling color: %d\n", map->ceiling_color);
-	printf("Width: %d\n", map->map_width);
-	printf("Height: %d\n", map->map_height);
-	printf("map: \n");
-	i = 0;
-	while (map->map[i])
-	{
-		printf("%s\n", map->map[i]);
-		i++;
-	}
-	printf("Player in map: (%d, %d)\n", map->player.map_pos.x, map->player.map_pos.x);
-	printf("Player in minimap: (%d, %d)\n", map->player.mini_pos.x, map->player.mini_pos.x);
-	printf("Player direction: %f rad --> %f deg\n", map->player.dir, map->player.dir * 180 / M_PI);
 }
